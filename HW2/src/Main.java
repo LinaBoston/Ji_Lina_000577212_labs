9233b4e988e765d6ecd5e942c3f56ecf4163a8a1
Lina <lina@DN0a1e851a.SUNet>
2024-10-16 10:42:04 -0700

    Assignment 2 file for main

---
+++ HW2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        javax.swing.SwingUtilities.invokeLater( new Runnable(){
+            @Override
+            public void run() {
+                MainJFrame frame = new MainJFrame();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
